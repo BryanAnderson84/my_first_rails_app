0ce86f0784bfb5522c85b70d99fc36d79681857a
Bryan Anderson <bryannobleanderson@live.com>
2016-08-16 15:02:18 -0600

    added delete feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,10 +29,16 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     if @post.save
-      redirect_to posts_path
+      redirect_to post_path
     else
       render :new
     end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
